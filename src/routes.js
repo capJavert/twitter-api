264b058c49285d9411589390bf2040af2e4f093a
capJavert <ante.baric3@gmail.com>
2018-06-03 14:43:44 +0200

    added response wrapping

--- src/routes.js
+++ src/routes.js
@@ -23,127 +23,131 @@ const appRouter = function (app) {
 
         app.post('/follow/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.follow(request.params.username))
+                return response.wrap(await twitterUser.follow(request.params.username))
             }
         })
 
         app.post('/unfollow/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.unfollow(request.params.username))
+                return response.wrap(await twitterUser.unfollow(request.params.username))
             }
         })
 
         app.post('/tweet', async function(request, response) {
             if(!request.body.text) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: text'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: text'})
             } else {
-                return response.send(await twitterUser.tweet(request.body.text))
+                return response.wrap(await twitterUser.tweet(request.body.text))
             }
         })
 
         app.post('/like-recent-tweets/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.likeRecentTweets(request.params.username))
+                return response.wrap(await twitterUser.likeRecentTweets(request.params.username))
             }
         })
 
         app.post('/like-tweet/:username/status/:id', async function(request, response) {
             if(!request.params.username || !request.params.id) {
-                return response.send({'status': 'error', 'message': 'missing a parameters: username or status id'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameters: username or status id'})
             } else {
-                return response.send(await twitterUser.like(request.params.username, request.params.id))
+                return response.wrap(await twitterUser.like(request.params.username, request.params.id))
             }
         })
 
         app.post('/like-last-tweet/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.likeLastTweet(request.params.username))
+                return response.wrap(await twitterUser.likeLastTweet(request.params.username))
             }
         })
 
         app.post('/follow-network/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.followNetwork(request.params.username))
+                return response.wrap(await twitterUser.followNetwork(request.params.username))
             }
         })
 
         app.post('/follow-interests/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.followInterests(request.params.username))
+                return response.wrap(await twitterUser.followInterests(request.params.username))
             }
         })
 
         app.get('/:username/followers', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.followers(request.params.username))
+                return response.wrap(await twitterUser.followers(request.params.username))
             }
         })
 
         app.get('/followers', async function(request, response) {
             if(!twitterUser.data.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.followers())
+                return response.wrap(await twitterUser.followers())
             }
         })
 
         app.get('/:username/interests', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.interests(request.params.username))
+                return response.wrap(await twitterUser.interests(request.params.username))
             }
         })
 
         app.get('/interests', async function(request, response) {
             if(!twitterUser.data.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.interests())
+                return response.wrap(await twitterUser.interests())
             }
         })
 
         app.post('/retweet/:username/status/:id', async function(request, response) {
             if(!request.params.username || !request.params.id) {
-                return response.send({'status': 'error', 'message': 'missing a parameters: username or status id'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameters: username or status id'})
             } else {
-                return response.send(await twitterUser.retweet(request.params.username, request.params.id))
+                return response.wrap(await twitterUser.retweet(request.params.username, request.params.id))
             }
         })
 
         app.post('/retweet-last/:username', async function(request, response) {
             if(!request.params.username) {
-                return response.send({'status': 'error', 'message': 'missing a parameter: username'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameter: username'})
             } else {
-                return response.send(await twitterUser.retweetLastTweet(request.params.username))
+                return response.wrap(await twitterUser.retweetLastTweet(request.params.username))
             }
         })
 
         app.post('/login', async function (request, response) {
             if(!request.body.username || !request.body.password) {
-                return response.send({'status': 'error', 'message': 'missing a parameters: username or password'})
+                return response.wrap({'status': 'error', 'message': 'missing a parameters: username or password'})
             } else {
-                return response.send(await twitter.login(request.body.username, request.body.password))
+                return response.wrap(await twitter.login(request.body.username, request.body.password))
             }
         })
 
         app.get('/logout', async function (request, response) {
-            return response.send(await twitter.logout())
+            return response.wrap(await twitter.logout())
         })
+
+        app.response.wrap = function (data) {
+            return this.send({data: data})
+        }
     })
 }
 
