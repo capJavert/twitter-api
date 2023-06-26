e5425761a3bef209597dac0221fce1eabeae63f1
capJavert <ante@kickass.website>
2023-06-26 20:23:34 +0200

    feat: pass headless option from cli

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-require("./src/routes.js")(app);
+let headless = process.argv[2] ?? 'new'
+
+if (headless === 'false') {
+    headless = false
+}
+
+require("./src/routes.js")(app, '', headless);
 
 const server = app.listen(3000, function () {
     console.log("NodeJS Twitter API Server listening on port %s...", server.address().port);
